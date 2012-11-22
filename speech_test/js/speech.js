90abebb12ab5ecc1bcbcea1c8a0786ae39f9f8aa
Kyle Tolle <kyle@nullsix.com>
2012-11-22 03:45:18 -0700

    Refactor code and phrases.
    Supported phrases:
      move left/right/up/down
      jump left/right/up/down
      move in/out
      way in/out
    
    Refactor code to be more reliable by matching on words that are more
    likely to be translated properly.
    Refactor code to contain less duplication and be more readable.

--- speech_test/js/speech.js
+++ speech_test/js/speech.js
@@ -17,78 +17,92 @@ window.onload = function () {
     mic.focus();
   };
 
-  function panMap(x,y)
-  {
-    map.panBy(L.point(x,y));
-  }
-
-  function magnitudeOfMotion(motion) {
-    return motion == "jump" ? 500 : 100
-  }
-
+  // Suppored phrases:
+  //   move up/down/left/right
+  //   jump up/down/left/right
+  //   move in/out
+  //   way in/out
   function processPhrase(phrase) {
-    var words     = phrase.split(" "),
-        motion    = words[0],
-        direction = words[1],
-        magnitude = magnitudeOfMotion(motion);
+    var words         = phrase.split(" "),
+        motion        = words[0],
+        direction     = words[1];
 
-    switch(motion) {
-      case 'move':
-        switch(direction) {
-          case 'left':
-            panMap(-magnitude, 0);
-            break;
+    if (isAZoomCommand(direction))
+    {
+      var zoomMagnitude = magnitudeOfZoom(motion);
+      processZoom(direction, zoomMagnitude);
 
-          case 'right':
-            panMap(magnitude, 0);
-            break;
-
-          case 'up':
-            panMap(0, -magnitude);
-            break;
-
-          case 'down':
-            panMap(0, magnitude);
-            break;
-
-          case 'in':
-            map.zoomIn();
-            break;
-
-          case 'out':
-            map.zoomOut();
-            break;
-        }
-        break;
-
-      case 'jump':
-        switch(direction) {
-          case 'left':
-            panMap(-magnitude, 0);
-            break;
-
-          case 'right':
-            panMap(magnitude, 0);
-            break
-
-          case 'up':
-            panMap(0, -magnitude);
-            break;
-
-          case 'down':
-            panMap(0, magnitude);
-            break;
-
-          case 'out':
-            map.setZoom(map.getZoom()-3);
-            break;
-          
-          case 'in':
-            map.setZoom(map.getZoom()+3);
-            break;
-        }
-        break;
+    } else {
+      var moveMagnitude = magnitudeOfMove(motion);
+      processDirection(direction, moveMagnitude);
     }
   };
+
+  // The following functions match against the words that are most
+  // likely to be translated correctly. This won't always result in
+  // the map reacting to a phrase, but it increases the chances
+  // greatly. This helps avoid instances where the translation is
+  // imprecise. This should give the user more feedback on whether
+  // their command was interpreted correctly, or whether the
+  // translation was off. This is better than the map not doing
+  // anything, and leaving the user wondering what happened.
+
+  function isAZoomCommand(direction) {
+    // These are words that are translated most reliably, so they are
+    // reliable to use for determining which command we've received.
+    return direction == 'in' || direction == 'out';
+  }
+
+  function magnitudeOfZoom(motion) {
+    // Matching on "way" allows us to ignore the fact that
+    // "zoom" is never translated correctly.
+    return motion == "way" ? 3 : 1;
+  }
+
+  function processZoom(direction, zoomMagnitude) {
+    // To zoom out, we need to invert the zoomMagnitude.
+    if (direction == 'out') {
+      zoomMagnitude *= -1;
+    }
+
+    map.setZoom(map.getZoom()+zoomMagnitude);
+  }
+
+  function magnitudeOfMove(motion) {
+    // Matching on "move" is more reliable than matching on "jump".
+    return motion == "move" ? 100 : 500;
+  }
+
+  function processDirection(direction, moveMagnitude) {
+    var coords;
+
+    switch(direction) {
+      case 'left':
+        coords = [-moveMagnitude, 0];
+        break;
+
+      case 'right':
+        coords = [moveMagnitude, 0];
+        break
+
+      case 'up':
+        coords = [0, -moveMagnitude];
+        break;
+
+      case 'down':
+        coords = [0, moveMagnitude];
+        break;
+      
+      default:
+        // If we didn't recognize the word, don't do anything.
+        return;
+    }
+
+  panBy(coords);
+  }
+
+  function panBy(coords) {
+    map.panBy(L.point(coords[0], coords[1]));
+  }
 };
 
